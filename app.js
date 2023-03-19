42067e034031d1e4d6a9e97402050a6892180673
Ramazan Mert Kaya <kmert0@outlook.com>
2023-03-20 00:03:09 +0300

    Adding new items

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get("/", function (req, res) {
           .catch(function (err) {
             console.log(err);
           });
-          res.redirect("/");
+        res.redirect("/");
       } else {
         res.render("list", { listTitle: "Today", newListItems: foundItems });
       }
@@ -55,15 +55,15 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName,
+  });
+
+  item.save();
+
+  res.redirect("/");
 });
 
 app.get("/work", function (req, res) {
