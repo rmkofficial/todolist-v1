15137ea11df06100151598ee06e19d4150b38422
Ramazan Mert Kaya <kmert0@outlook.com>
2023-03-19 23:51:36 +0300

    Rendering database items

--- app.js
+++ app.js
@@ -2,53 +2,56 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-
 const app = express();
-
-
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+});
 
 const itemsSchema = {
-  name: String
+  name: String,
 };
 
 const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item ({
-  name: "Welcome to your todolist!"
+const item1 = new Item({
+  name: "Welcome to your todolist!",
 });
 
-const item2 = new Item ({
-  name: "Hit the + button to aff a new item"
+const item2 = new Item({
+  name: "Hit the + button to aff a new item",
 });
 
-const item3 = new Item ({
-  name: "<-- Hit this to delete an item"
+const item3 = new Item({
+  name: "<-- Hit this to delete an item",
 });
 
 const defaultItems = [item1, item2, item3];
 
-
-
-Item.insertMany(defaultItems)
-      .then(function () {
-        console.log("Successfully saved defult items to DB");
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-
 app.get("/", function (req, res) {
-  
-  
-
-  res.render("list", { listTitle: "Today", newListItems: items });
+  Item.find()
+    .then(function (foundItems) {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems)
+          .then(function () {
+            console.log("Successfully saved defult items to DB");
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+          res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });
 
 app.post("/", function (req, res) {
@@ -73,9 +76,9 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 });
 
-app.get("/about", function(req,res){
+app.get("/about", function (req, res) {
   res.render("about");
-})
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
